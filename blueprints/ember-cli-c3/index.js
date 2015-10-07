d2f1452b9ea184aab272d58bbaedbe73ba99c996
Andrew Walton <andrew.walton@cordatahealth.com>
2015-10-07 07:39:35 -0400

    Adding ember-c3-shim to blueprint and cleaning up NPM / bower deps

--- blueprints/ember-cli-c3/index.js
+++ blueprints/ember-cli-c3/index.js
@@ -3,10 +3,6 @@ module.exports = {
   description: 'Include dependent bower packages.',
 
   afterInstall: function(options) {
-    var that = this;
-
-    return that.addBowerPackageToProject('c3').then(function() {
-      return that.addBowerPackageToProject('d3', '<=3.5.0');
-    });
+    return this.addAddonToProject('ember-c3-shim');
   }
 };
